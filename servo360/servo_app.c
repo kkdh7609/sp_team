31bdbfc611b2c335a8c7b4cb7216ef512c0ed630
DOHYEON <kkdh7609@ajou.ac.kr>
2018-11-30 07:52:25 +0000

    servo360

---
+++ servo360/servo_app.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+
+int main(void){
+  int fd,fp;
+  char k;
+
+  fd = open("/dev/servo_dev", O_RDWR);
+
+  if(fd < 0){
+    perror("Failed to open the device..");
+    return errno;
+  }
+
+  k = '2';
+  fp = write(fd, &k, 1);
+
+  return 0;
+}
+
