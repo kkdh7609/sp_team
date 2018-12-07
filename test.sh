aef6fb54570d3c2b3061f0b0255c8407b7a0c653
kkdh7609 <kkdh7609@ajou.ac.kr>
2018-12-08 06:19:46 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,13 +1,7 @@
-cd button
-sudo insmod button_dev.ko
-sudo mknod -m 666 /dev/button_dev c 
-cd ..
-cd light
-sudo insmod light_dev.ko
-sudo mknod -m 666 /dev/light_dev c
-cd ..
-cd servo180
-sudo insmod servo180_dev.ko
-sudo mknod -m 666 /dev/servo180_dev c
-cd ..
-cd main
+#!/bin/bash
+sudo insmod ./button/button_dev.ko
+sudo insmod ./button/button_dev_2.ko
+sudo mknod -m 666 /dev/button_dev c 239 0
+sudo mknod -m 666 /dev/button_dev_2 c 238 0
+sudo insmod ./light/light_dev.ko
+sudo mknod -m 666 /dev/light_dev c 243 0
