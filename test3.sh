077c687e0c3fa85f7cc6938fe6776c2e05efc667
kkdh7609 <kkdh7609@ajou.ac.kr>
2018-12-08 06:22:36 +0900

    Update test3.sh

--- test3.sh
+++ test3.sh
@@ -1,9 +1,9 @@
-cd button
-sudo insmod button_dev.ko
-sudo mknod -m 666 /dev/button_dev c 239 0
-cd ..
-cd light
-sudo insmod light_dev.ko
-sudo mknod -m 666 /dev/light_dev c 243 0
-cd ..
-cd main
+#!/bin/bash
+sudo insmod ./gas/gas_dev.ko
+sudo mknod -m 666 /dev/gas_dev c 240 0
+sudo insmod ./Soilmoisture/Soilmoisture_dev.ko
+sudo insmod ./Soilmoisture/Soilmoisture_dev_2.ko
+sudo mknod -m 666 /dev/soil_dev c 241 0
+sudo mknod -m 666 /dev/soil_dev c 242 0
+sudo insmod ./dht/dht_dev.ko
+sudo mknod -m 666 /dev/dht_dev c 239 0
