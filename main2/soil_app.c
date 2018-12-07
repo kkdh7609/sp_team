785e40b26c77277e0d2cedb0478556ef14df1c83
kkdh7609 <kkdh7609@ajou.ac.kr>
2018-12-07 15:13:32 +0900

    Update soil_app.c
    soil_app c edit

--- main2/soil_app.c
+++ main2/soil_app.c
@@ -3,7 +3,7 @@
 #include <fcntl.h>
 
 #define SOIL1_DEV_NAME "/dev/soil_dev"
-#define SOIL2_DEV_NAME "/dev/soil_dev"
+#define SOIL2_DEV_NAME "/dev/soil_dev_2"
 
 // output
 int status_soil(int mode){
